27fcf06f3725bf92e73b76758e80398e328773ac
Preethi K S <preethikarthi008@gmail.com>
2026-08-15 22:08:16 +0530

    Add files via upload

---
+++ pkaccount/Account.java
@@ -0,0 +1,19 @@
+package pkaccount;
+public abstract class Account 
+{
+protected int accnumber;
+protected double balance;
+
+public Account()
+{
+accnumber=0;
+balance=0;
+}
+
+
+public Account(int accnumber , double balance)
+{
+this.accnumber=accnumber;
+this.balance=balance;
+}
+}
