61a6230ea1cee6d1264de1a033c69af26a5f10ed
Preethi K S <preethikarthi008@gmail.com>
2026-08-15 22:02:34 +0530

    Add files via upload

---
+++ Interface/InterestRate.java
@@ -0,0 +1,9 @@
+package pkinterface;
+public interface InterestRate
+{
+ 
+ double sbrate=0.04;
+ double fdrate=0.0825;
+}
+
+ 
---
+++ Interface/Transaction.java
@@ -0,0 +1,11 @@
+package pkinterface;
+public interface Transaction 
+{
+ int min_balance=500;
+
+
+ void withdraw(double amount);
+ void deposit(double amount);
+
+}
+
